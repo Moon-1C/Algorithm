2287fa6ce4aaec759aeb12ca5395596d0865a51d
kkki11 <118742845+Moon-1C@users.noreply.github.com>
2023-10-18 00:41:42 +0900

    [Gold IV] Title: 네트워크 연결, Time: 540 ms, Memory: 51636 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -0,0 +1,109 @@
+import java.security.Key;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.valueOf(br.readLine());
+
+        int M = Integer.valueOf(br.readLine());
+
+        arr = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            arr[i] = i;
+        }
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int node1 = Integer.valueOf(st.nextToken());
+            int node2 = Integer.valueOf(st.nextToken());
+            int weight = Integer.valueOf(st.nextToken());
+
+            pq.add(new Edge(node1, node2, weight));
+
+
+        }
+
+        int sum = 0;
+        while (!pq.isEmpty()) {
+            Edge now = pq.poll();
+            if (find(now.node1) != find(now.node2)) {
+                union(now.node1, now.node2);
+                sum += now.weight;
+            }
+
+        }
+        System.out.println(sum);
+
+
+
+
+
+
+
+
+    }
+
+    static class Edge implements Comparable<Edge> {
+
+
+        int node1;
+        int node2;
+        int weight;
+
+        public Edge(int node1, int node2, int weight) {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.weight = weight;
+        }
+
+
+
+        @Override
+        public int compareTo(Edge o) {
+            return weight - o.weight;
+        }
+    }
+
+
+
+
+
+
+
+    static void union(int a, int b) {
+
+        a = find(a);
+        b = find(b);
+
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (a != b) {
+            arr[b] = a;
+        }
+
+
+    }
+
+    static int find(int a ) {
+        if (arr[a] == a) {
+            return a;
+        } else {
+            return find(arr[a]);
+        }
+
+
+    }
+
+
+}
